9fdbc21c8b3297fac86d3a95f06be319dd4720e9
user8394 <111243906+user8394@users.noreply.github.com>
2022-08-16 11:22:20 +0100

    Create test.py

---
+++ streamlit_test/pages/test.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from transformers import pipeline
+
+load_pipeline = st.cache(pipeline)
+
+
+
+st.title('Test app page 2')
+
+
+st.write('Loading model...')
+
+model = load_pipeline('zero-shot-classification')
+
+st.write('Done ✅')
