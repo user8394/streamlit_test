d7b92cbf653cee07968111083a1d8c0754069a9d
user8394 <111243906+user8394@users.noreply.github.com>
2022-08-16 11:31:25 +0100

    Update test.py

--- streamlit_test/pages/test.py
+++ streamlit_test/pages/test.py
@@ -10,6 +10,55 @@ st.title('Test app page 2')
 
 st.write('Loading model...')
 
-#model = load_pipeline('zero-shot-classification')
+zero_shot_classifier = load_pipeline('zero-shot-classification')
+
+
+class ZeroShotClassification:
+
+	def __init__(self, classifier=None):
+		self.classifier = classifier or pipeline('zero-shot-classification')
+
+
+	def __call__(self, sentence):
+		return self.predict(sentence)
+
+
+	def get_label_score(self, sentence, label, labels):
+
+		result = self.classifier(sentence, labels)
+		label_index = result['labels'].index(label)
+		label_score = result['scores'][label_index]
+
+		return label_score
+
+
+	def predict(self, sentence):
+
+		# negativity_score = self.get_label_score(sentence, 'negative', [
+		# 	'negative',
+		# 	'positive',
+		# 	'neutral',
+		# ])
+
+
+		# if negativity_score < 0.7:
+		# 	return (False, None)
+
+
+		return self.classifier(sentence, [
+			'suicide',
+			'sexual abuse',
+			'physical abuse',
+			'self harm',
+      'violence',
+      'feeling unsafe',
+			'other'
+		])
+  
+ 
 
 st.write('Done ✅')
+
+cls = ZeroShotClassifier(zero_shot_classifier)
+
+st.write(cls('I want to hurt myself'))
