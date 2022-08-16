b6f136ae633a36b4c7b3003040c93175198e43bc
user8394 <111243906+user8394@users.noreply.github.com>
2022-08-16 14:18:04 +0100

    Create polarity_classification.py

---
+++ streamlit_test/pages/polarity_classification.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import spacy
+from spacytextblob.spacytextblob import SpacyTextBlob
+
+nlp = spacy.load("en_core_web_sm")
+nlp.add_pipe('spacytextblob')
+
+
+def polarity(text):
+  return nlp(text)._.polarity
+
+
+st.title('Polarity Classification')
+
+st.write('''
+This model uses spaCy to predict the polarity (degree of positive or negative emotion) within a piece of text. This can be used to flag potentially extreem of concerning content 
+within user responses. The advantage of this model over NLI is simplicity and inference speed.
+''')
+
+input = st.textarea('Input sentence')
+
+with st.spinner('Analysing...'):
+  score = polarity(input)
+
+st.success('Analysing complete ✅')
+st.write('#### Result')
+st.metric("Polarity", str(score) + '%')
+
